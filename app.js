fd1e9346e596b6874444633510c60c21ef5571d3
Ali Ramazanov <aliasifzade@gmail.com>
2023-10-03 19:54:06 +0400

    Initialize Project

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+
+const pets = [
+  {
+    id: 1,
+    name: leblequja,
+  },
+];
+
+app.get("./pets", (req, res) => {
+  return res.json(pets);
+});
+
+app.get("./pets/:id", (req, res) => {
+  return res.json(pets.find(({ id }) => id === +req.params.id));
+});
+
+app.listen(9595, () => {
+  console.log(`Server is up & running on ${"http://localhost:9595"}`);
+});
